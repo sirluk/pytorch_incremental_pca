601d10ed75213e3b041085ee9971988b7ddea048
sirluk <lukas.hauzenberger@gmail.com>
2024-08-06 13:37:23 +0200

    simplify import

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# pytorch_incremental_pca
-A GPU-compatible PyTorch implementation of Incremental PCA for memory-efficient dimensionality reduction on large datasets.
+# PyTorch Incremental PCA
+
+[![PyPI Version](https://img.shields.io/pypi/v/torch-incremental-pca.svg)](https://pypi.org/project/torch-incremental-pca/)
+[![License](https://img.shields.io/badge/license-MIT-blue.svg)](LICENSE)
+[![Tests](https://github.com/sirluk/torch-incremental-pca/actions/workflows/tests.yml/badge.svg)](https://github.com/sirluk/torch-incremental-pca/actions/workflows/tests.yml)
+
+This project provides a PyTorch implementation of the Incremental PCA algorithm, inspired by the `IncrementalPCA` class from scikit-learn and this repository: [PCAonGPU](https://github.com/dnhkng/PCAonGPU/tree/main) The repository has some shortcomings with regards to the precision of operations leading to vastly different results compared to the sklearn implemtation. The IncrementalPCA class in this repo produces outputs which are very close to the sklearn implementation with the added benefit of running on GPU.
+
+Incremental PCA is a valuable technique for dimensionality reduction when dealing with large datasets that cannot fit entirely into memory.
+
+## Features
+
+* **PyTorch Integration:** Seamlessly use incremental PCA within your PyTorch workflows.
+* **Memory Efficiency:**  Process large datasets incrementally without loading everything into memory at once.
+* **Similar API:**  Familiar interface if you've used scikit-learn's `IncrementalPCA`.
+* **Customization:** Easily extend or modify the core functionality to suit your specific needs.
+
+## Installation
+
+```bash
+pip install torch-incremental-pca
+```
+
+## Usage
+```python
+import torch_incremental_pca as tip
+
+pca = tip.IncrementalPCA(n_components=32, device="cuda")
+```
---
+++ torch_incremental_pca/incremental_pca.py
@@ -0,0 +1,244 @@
+
+import torch
+
+
+class IncrementalPCAonGPU():
+    """
+    An implementation of Incremental Principal Components Analysis (IPCA) that leverages PyTorch for GPU acceleration.
+
+    This class provides methods to fit the model on data incrementally in batches, and to transform new data 
+    based on the principal components learned during the fitting process.
+
+    Attributes:
+        n_components (int, optional): Number of components to keep. If `None`, it's set to the minimum of the 
+                                      number of samples and features. Defaults to None.
+        whiten (bool): When True, the `components_` vectors are divided to ensure uncorrelated outputs with 
+                       unit component-wise variances. Defaults to False.
+        copy (bool): If False, input data will be overwritten. Defaults to True.
+        batch_size (int, optional): The number of samples to use for each batch. If `None`, it's inferred from 
+                                    the data and set to `5 * n_features`. Defaults to None.
+    """
+
+    def __init__(self, n_components=None, *, whiten=False, copy=True, batch_size=None, device="cpu"):
+        self.n_components = n_components
+        self.whiten = whiten
+        self.copy = copy
+        self.batch_size = batch_size
+
+        if self.device != "cpu":
+            assert torch.cuda.is_available(), f"CUDA is not available but device is {device}. Please set device='cpu' or install PyTorch with CUDA support."
+        self.device = torch.device(self.device)
+
+        # Set n_components_ based on n_components if provided
+        if n_components:
+            self.n_components_ = n_components
+
+        # Initialize attributes to avoid errors during the first call to partial_fit
+        self.mean_ = None  # Will be initialized properly in partial_fit based on data dimensions
+        self.var_ = None  # Will be initialized properly in partial_fit based on data dimensions
+        self.n_samples_seen_ = torch.tensor([0], device=self.device)
+
+    def _validate_data(self, X, dtype=torch.float32, copy=True):
+        """
+        Validates and converts the input data `X` to the appropriate tensor format.
+
+        This method ensures that the input data is in the form of a PyTorch tensor and resides on the correct device (CPU or GPU). 
+        It also provides an option to create a copy of the tensor, which is useful when the input data should not be overwritten.
+
+        Args:
+            X (Union[np.ndarray, torch.Tensor]): Input data which can be a numpy array or a PyTorch tensor.
+            dtype (torch.dtype, optional): Desired data type for the tensor. Defaults to torch.float32.
+            copy (bool, optional): Whether to clone the tensor. If True, a new tensor is returned; otherwise, the original tensor 
+                                   (or its device-transferred version) is returned. Defaults to True.
+
+        Returns:
+            torch.Tensor: Validated and possibly copied tensor residing on the specified device.
+        """
+        if not isinstance(X, torch.Tensor):
+            X = torch.tensor(X, dtype=dtype).to(self.device)
+        elif X.device != self.device:
+            X = X.to(self.device)
+        if copy:
+            X = X.clone()
+        return X
+
+    @staticmethod
+    def _incremental_mean_and_var(X, last_mean, last_variance, last_sample_count):
+        """
+        Computes the incremental mean and variance for the data `X`.
+
+        Args:
+            X (torch.Tensor): The batch input data tensor with shape (n_samples, n_features).
+            last_mean (torch.Tensor): The previous mean tensor with shape (n_features,).
+            last_variance (torch.Tensor): The previous variance tensor with shape (n_features,).
+            last_sample_count (torch.Tensor): The count tensor of samples processed before the current batch.
+
+        Returns:
+            Tuple[torch.Tensor, torch.Tensor, int]: Updated mean, variance tensors, and total sample count.
+        """
+        if X.shape[0] == 0:
+            return last_mean, last_variance, last_sample_count
+        
+        if last_sample_count > 0:
+            assert last_mean is not None, "last_mean should not be None if last_sample_count > 0."
+            assert last_variance is not None, "last_variance should not be None if last_sample_count > 0."
+
+        new_sample_count = torch.tensor([X.shape[0]], device=X.device)
+        updated_sample_count = last_sample_count + new_sample_count
+
+        if last_mean is None:
+            last_sum = torch.zeros(X.shape[1], dtype=torch.float64, device=X.device)
+        else:
+            last_sum = last_mean * last_sample_count
+
+        new_sum = X.sum(dim=0, dtype=torch.float64)
+        
+        updated_mean = (last_sum + new_sum) / updated_sample_count
+
+        T = new_sum / new_sample_count
+        temp = X - T
+        correction = temp.sum(dim=0, dtype=torch.float64).square()
+        temp.square_()
+        new_unnormalized_variance = temp.sum(dim=0, dtype=torch.float64)
+        new_unnormalized_variance -= correction / new_sample_count
+        if last_variance is None:
+            updated_variance = new_unnormalized_variance / updated_sample_count
+        else:
+            last_unnormalized_variance = last_variance * last_sample_count
+            last_over_new_count = last_sample_count.double() / new_sample_count
+            updated_unnormalized_variance = (
+                last_unnormalized_variance
+                + new_unnormalized_variance
+                + last_over_new_count
+                / updated_sample_count
+                * (last_sum / last_over_new_count - new_sum).square()
+            )
+            updated_variance = updated_unnormalized_variance / updated_sample_count
+
+        return updated_mean, updated_variance, updated_sample_count
+
+    @staticmethod
+    def _svd_flip(u, v, u_based_decision=True):
+        """
+        Adjusts the signs of the singular vectors from the SVD decomposition for deterministic output.
+
+        This method ensures that the output remains consistent across different runs.
+
+        Args:
+            u (torch.Tensor): Left singular vectors tensor.
+            v (torch.Tensor): Right singular vectors tensor.
+            u_based_decision (bool, optional): If True, uses the left singular vectors to determine the sign flipping. Defaults to True.
+
+        Returns:
+            Tuple[torch.Tensor, torch.Tensor]: Adjusted left and right singular vectors tensors.
+        """
+        if u_based_decision:
+            max_abs_cols = torch.argmax(torch.abs(u), dim=0)
+            signs = torch.sign(u[max_abs_cols, range(u.shape[1])])
+        else:
+            max_abs_rows = torch.argmax(torch.abs(v), dim=1)
+            signs = torch.sign(v[range(v.shape[0]), max_abs_rows])
+        u *= signs.view(1, -1)
+        v *= signs.view(-1, 1)
+        return u, v
+    
+    def fit(self, X, check_input=True):
+        """
+        Fits the model with data `X` using minibatches of size `batch_size`.
+
+        Args:
+            X (torch.Tensor): The input data tensor with shape (n_samples, n_features).
+
+        Returns:
+            IncrementalPCAGPU: The fitted IPCA model.
+        """
+        if check_input:
+            X = self._validate_data(X)
+        n_samples, n_features = X.shape
+        if self.batch_size is None:
+            self.batch_size_ = 5 * n_features
+        else:
+            self.batch_size_ = self.batch_size
+
+        for start in range(0, n_samples, self.batch_size_):
+            end = min(start + self.batch_size_, n_samples)
+            X_batch = X[start:end]
+            self.partial_fit(X_batch, check_input=False)
+
+        return self
+
+    def partial_fit(self, X, check_input=True):
+        """
+        Incrementally fits the model with batch data `X`.
+
+        Args:
+            X (torch.Tensor): The batch input data tensor with shape (n_samples, n_features).
+            check_input (bool, optional): If True, validates the input. Defaults to True.
+
+        Returns:
+            IncrementalPCAGPU: The updated IPCA model after processing the batch.
+        """
+        first_pass = not hasattr(self, "components_")
+
+        if check_input:
+            X = self._validate_data(X)
+        n_samples, n_features = X.shape
+
+        if first_pass:
+            self.components_ = None
+        if self.n_components is None:
+            self.n_components_ = min(n_samples, n_features)
+
+        col_mean, col_var, n_total_samples = self._incremental_mean_and_var(
+            X, self.mean_, self.var_, self.n_samples_seen_
+        )
+
+        # Whitening
+        if self.n_samples_seen_ == 0:
+            X -= col_mean
+        else:
+            col_batch_mean = torch.mean(X, dim=0)
+            X -= col_batch_mean
+            mean_correction_factor = torch.sqrt((self.n_samples_seen_.double() / n_total_samples) * n_samples)
+            mean_correction = mean_correction_factor * (self.mean_ - col_batch_mean)
+            X = torch.vstack(
+                (
+                    self.singular_values_.view((-1, 1)) * self.components_,
+                    X,
+                    mean_correction,
+                )
+            )
+
+        U, S, Vt = torch.linalg.svd(X, full_matrices=False, driver="gesvd")
+        U, Vt = self._svd_flip(U, Vt, u_based_decision=False)
+        explained_variance = S**2 / (n_total_samples - 1)
+        explained_variance_ratio = S**2 / torch.sum(col_var * n_total_samples)
+
+        self.n_samples_seen_ = n_total_samples
+        self.components_ = Vt[: self.n_components_]
+        self.singular_values_ = S[: self.n_components_]
+        self.mean_ = col_mean
+        self.var_ = col_var
+        self.explained_variance_ = explained_variance[: self.n_components_]
+        self.explained_variance_ratio_ = explained_variance_ratio[: self.n_components_]
+        if self.n_components_ not in (n_samples, n_features):
+            self.noise_variance_ = explained_variance[self.n_components_ :].mean()
+        else:
+            self.noise_variance_ = torch.tensor(0.0, device=self.device)
+        return self
+
+    def transform(self, X):
+        """
+        Applies dimensionality reduction to `X`.
+
+        The input data `X` is projected on the first principal components previously extracted from a training set.
+
+        Args:
+            X (torch.Tensor): New data tensor with shape (n_samples, n_features) to be transformed.
+
+        Returns:
+            torch.Tensor: Transformed data tensor with shape (n_samples, n_components).
+        """
+        X = X.to(self.device)
+        X -= self.mean_
+        return torch.mm(X, self.components_.T)
